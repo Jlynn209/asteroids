69cc4d42619626fa2d9a2cd0c5347b61ae1fe457
Jlynn209 <jlynn_209@aol.com>
2025-12-06 14:21:40 -0800

    Add CircleShape and Player classes with basic functionality

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    clock = pygame.time.Clock()
+    delta_time = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,9 +26,13 @@ def main():
                 return
         screen.fill("black")
 
+        updatable.update(delta_time)
+        
+        for sprite in drawable:
+            sprite.draw(screen)
 
+        delta_time = clock.tick(60) / 1000.0
         pygame.display.flip()
-
 
 
 if __name__ == "__main__":
